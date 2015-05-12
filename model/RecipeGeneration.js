69fbc8f76ae153d08afc28f3b9bffd9e862c6702
alourdu <alourdu@alourdult-01.dsa.mot-mobility.com>
2015-05-12 21:56:57 +0530

    Test

--- model/RecipeGeneration.js
+++ model/RecipeGeneration.js
@@ -1,23 +1,74 @@
 var mongoose = require('mongoose');
 var MongoClient = require('mongodb').MongoClient;
 var Schema = mongoose.Schema;
+var RecipeSchema = new Schema({
+	ID: Number,
+	FILENAME: String,	
+	VERSION: Number,
+	RECIPE_FILE: String,	
+	EXTRANET_URL: String,
+	STATUS: Number,
+	ERROR_MESSAGE: String,
+	MT_TESTCASES_ID: Number,
+	CREATED_BY: String,
+	CREATED_DATE: Date,
+	MODIFIED_BY: String,
+	MODIFIED_DATE: Date,
+	EXTRANET_FILE_NAME: String
 
+});
+
+var RecipeStepsSchema = new Schema({
+	MT_RECIPEXML_NEW_ID:	Number,
+	STEP_Number:	Number,
+	STEP_DETAILS:	String,
+	FRONTEND:	String,
+	BACKEND:	String,
+	AUDIO_ALERT:	String,
+	BATTERY:	String,
+	BLUETOOTH:	String,	
+	CAMERA:	String,
+	CHARGING:	String,
+	DISPLAY_KEYBORED:	String,
+	MEMORY_CARD:	String,
+	POWER_ON_OFF:	String,
+	SIM_CARD:	String,
+	WIFI: String,
+	NO_COMPLAINT:	String
+	
+	});
+
+
+
+
+var XUser = mongoose.model('MT_RECIPEXML', RecipeSchema);
+//recipexml table insert new recipe file name
+
+
+var SUser = mongoose.model('MT_RECIPE_STEPS', RecipeStepsSchema);
+// steps table insert the recipes details as each record based on the test cases			
+
+
+//chethana 
 exports.showRecipePage = function (req, res) {
 	MongoClient.connect("mongodb://localhost/RecipeGenerationDB", function(err, db) {
 
 		if(err) { return console.dir(err); }
 
-		console.log("Connected to db");
-		var collection = db.collection('mt_testcases');
+		console.log("Connected to mt_recipexmls db");
+		var collection = db.collection('mt_recipexmls');
 										
 		console.log("Data selected from DB for drop down");
 		collection.find({STATUS:0}).toArray(function(err, items) {
 			console.log("items : ",items);
+			
+		var collection2 = db.collection('mt_testcases');
+		console.log("Data selected from DB for testcase tab");
 						
-		var stream = collection.find({STATUS:0}).stream();
-		 stream.on("data", function(item) {
-			 console.log("item : ",item.TESTCASE_NAME);
-			 res.render('RecipeGeneration',{ item: items , testCaseName: item.TESTCASE_NAME});
+		var stream = collection2.find({STATUS:0}).stream();
+		stream.on("data", function(item) {
+		console.log("item : ",item.TESTCASE_NAME);
+		res.render('RecipeGeneration',{ item: items , testCaseName: item.TESTCASE_NAME});
 			
 		 });
 		 stream.on("end", function() {}); 
@@ -25,6 +76,8 @@ exports.showRecipePage = function (req, res) {
 	});
 	
 };
+
+//
 
 exports.loadTestcases = function (req, res) {
 	MongoClient.connect("mongodb://localhost/RecipeGenerationDB", function(err, dbs) {
@@ -50,6 +103,13 @@ exports.loadTestcases = function (req, res) {
 };
 
 exports.saveRecipe = function(req, res){
+	
+
+	
+	
+	
+	
+	
 	var form = req.form;
 	console.log("form details" +form);
 	mongoose.connect('mongodb://localhost/RecipeGenerationDB', function (err, res) {
@@ -58,31 +118,35 @@ exports.saveRecipe = function(req, res){
 		} else {
 		console.log ('Succeeded connected to: ' + 'mongodb://localhost/RecipeGenerationDB');
 		}
+		
 		});
 	
-	// recipexml table insert new recipe file name
-	var RecipeSchema = new Schema({
-		ID: Number,
-		FILENAME: String,	
-		VERSION: Number,
-		RECIPE_FILE: String,	
-		EXTRANET_URL: String,
-		STATUS: Number,
-		ERROR_MESSAGE: String,
-		MT_TESTCASES_ID: Number,
-		CREATED_BY: String,
-		CREATED_DATE: Date,
-		MODIFIED_BY: String,
-		MODIFIED_DATE: Date,
-		EXTRANET_FILE_NAME: String
+	
+	
+	
+	
+	
+	
+	/*
+	 * get the max(id) from MT_RECIPEXML assign it a var incriment it and assign to new variable if number of record is zero then assign i=0
+	 */
+	
+	var recipeXMLCount=0;
+	var recipeXMLID=0; 
 
-	});
-
-	var XUser = mongoose.model('MT_RECIPEXML', RecipeSchema);
+	
+	XUser.find(function (err, kittens) {
+		  if (err) return console.error(err);
+		  recipeXMLCount= kittens.length;
+		  
+		  recipeXMLID=recipeXMLCount+1;
+		  console.log("recipeXMLID"+recipeXMLID);
+		
+	
 		var recipeFileName =req.query.recipeFileName;
-		console.log("recipeFileName"+ recipeFileName);
+		console.log("recipeFileName"+ recipeXMLID);
 		var xmldoe = new XUser ({
-				ID: '301',
+				ID: recipeXMLID,
 				FILENAME:recipeFileName,	
 				VERSION: '0',
 				RECIPE_FILE: 'recipefile.xml',//steps details	
@@ -105,61 +169,46 @@ exports.saveRecipe = function(req, res){
 				console.log("generateJsonString : " + generatedJsonString);
 				
 				var recipes = JSON.parse(generatedJsonString);
-				console.log("recipies"+ recipes);
+				//console.log("recipies"+ recipes);
 				
 
-				
-	// steps table insert the recipes details as each record based on the test cases			
+						
 
-	var RecipeStepsSchema = new Schema({
-		MT_RECIPEXML_NEW_ID:	Number,
-		STEP_Number:	Number,
-		STEP_DETAILS:	String,
-		FRONTEND:	String,
-		BACKEND:	String,
-		AUDIO_ALERT:	String,
-		BATTERY:	String,
-		BLUETOOTH:	String,	
-		CAMERA:	String,
-		CHARGING:	String,
-		DISPLAY_KEYBORED:	String,
-		MEMORY_CARD:	String,
-		POWER_ON_OFF:	String,
-		SIM_CARD:	String,
-		WIFI: String,
-		NO_COMPLAINT:	String
-		});
 	
-	var SUser = mongoose.model('MT_RECIPE_STEPS', RecipeStepsSchema);
+	
 	console.log("req.query.TClength"+ req.query.TClength);		  
 	for(var i=0; i<req.query.TClength; i++)
 	{
 
 	var stepsdoe = new SUser ({
-		MT_RECIPEXML_NEW_ID:'1001',
-		STEP_Number:'0',
-		STEP_DETAILS:'testcase12',	
+		MT_RECIPEXML_NEW_ID:recipeXMLID,
+		STEP_Number:i,
+		STEP_DETAILS:recipes[i].testCase,	
 		FRONTEND: recipes[i].frontEnd,
 		BACKEND:	recipes[i].backEnd,
-		AUDIO_ALERT: recipes[i].complaint,
-		BATTERY:	recipes[i].audioAlert,
-		BLUETOOTH:	recipes[i].battery,	
-		CAMERA:	recipes[i].bluetooth,
-		CHARGING:	recipes[i].camera,
-		DISPLAY_KEYBORED:	recipes[i].charging,
-		MEMORY_CARD:	recipes[i].displayKeypad,
-		POWER_ON_OFF:	recipes[i].memoryCard,
-		SIM_CARD:	recipes[i].powerOnOf,
-		WIFI:recipes[i].simCard,
-		NO_COMPLAINT:recipes[i].wifi
+		AUDIO_ALERT: recipes[i].audioAlert,
+		BATTERY:	recipes[i].battery,
+		BLUETOOTH:	recipes[i].bluetooth,	
+		CAMERA:	recipes[i].camera,
+		CHARGING:	recipes[i].charging,
+		DISPLAY_KEYBORED:	recipes[i].displayKeypad,
+		MEMORY_CARD:	recipes[i].memoryCard,
+		POWER_ON_OFF:	recipes[i].powerOnOf,
+		SIM_CARD:	recipes[i].simCard,
+		WIFI:recipes[i].wifi,
+		NO_COMPLAINT:recipes[i].complaint
 		});
 
 	stepsdoe.save(function (err) {if (err) console.log ('Error on save!')});
 	
 	
-	}
 	
-	res.end();
+	}
+	})	
+	res.render('RecipeGeneration',{'title':'Successfully saved Recipe!!!'})
+	
+	
+	
 
 	
 };
